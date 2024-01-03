36d29ff76eb1de10377cb3c7018a7db04bef8b78
lsmda <lsmda@apollo.pm>
2024-01-03 21:07:04 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,6 +22,14 @@ ln -s ~/.dotfiles/tmux/ ~/.config/tmux
 
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+
+tar xf lazygit.tar.gz lazygit
+
+sudo install lazygit /usr/local/bin
+
 # Download the latest Neovim AppImage
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 
