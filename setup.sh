4115ca71b8919768498e11c65b862b29fabc9442
lsmda <lsmda@apollo.pm>
2024-01-24 09:50:05 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y git libevent-dev ncurses-dev \
   xclip make pkg-config nodejs npm tmux
 
 # Create necessary directories
-mkdir -p ~/.config
+mkdir -p /.config
 
 # Clone necessary repositories
 git clone https://github.com/lsmda/nvim /.config/nvim
@@ -20,8 +20,7 @@ ln -s /.dotfiles/tmux/ /.config/tmux
 # Download and install Neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x /nvim.appimage
-~/nvim.appimage --appimage-extract
-sudo mv /squashfs-root /
+/nvim.appimage --appimage-extract
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 rm /nvim.appimage
 
