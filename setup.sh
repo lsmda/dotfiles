b53c1f2a5e08d0a24dc328cc2c90978572ff9bc7
lsmda <lsmda@apollo.pm>
2024-01-24 10:39:30 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,6 +29,12 @@ sudo mv ~/squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 rm ~/nvim.appimage
 
+# Download and install LazyGit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
 # Check Neovim version
 /squashfs-root/AppRun --version
 
