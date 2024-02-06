67efafb74d9154aadc6a7392c01aab9f91ad4397
lsmda <lsmda@apollo.pm>
2024-02-06 21:43:40 +0000

    update tmux

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+set -g fish_greeting ""
+
+# Check if TMUX is unset or empty and if the session is interactive
+if test -z "$TMUX" && status is-interactive
+    # Check if any Tmux sessions exist
+    set tmux_sessions (tmux list-sessions)
+    if test -z "$tmux_sessions"
+        # If no sessions exist, start a new Tmux session
+        tmux new-session
+    else
+        # If sessions exist, attach to the first one
+        tmux attach-session -t (echo $tmux_sessions[1] | cut -d: -f1)
+    end
+end
