d0d6d6bf3296c460262929197c10e404fb97cab3
lsmda <contact@lsmda.pm>
2024-08-28 00:45:59 +0100

    remove tmux and update kitty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,16 +10,3 @@ alias mt="mount"
 alias umt="umount"
 alias files="sudo mount -t nfs 10.0.0.5:/files /mnt/files/"
 alias media="sudo mount -t nfs 10.0.0.5:/media /mnt/media/"
-
-# Check if TMUX is unset or empty and if the session is interactive
-if test -z "$TMUX" && status is-interactive
-  # Check if any Tmux sessions exist
-  set tmux_sessions (tmux list-sessions)
-  if test -z "$tmux_sessions"
-    # If no sessions exist, start a new Tmux session
-    tmux new-session
-  else
-    # If sessions exist, attach to the first one
-    tmux attach-session -t (echo $tmux_sessions[1] | cut -d: -f1)
-  end
-end
