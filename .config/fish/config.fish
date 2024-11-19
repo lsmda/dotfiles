1381283cced1dffa109e6076cba0a8be57fdcb93
lsmda <contact@lsmda.pm>
2024-11-19 20:12:04 +0000

    recover tmux config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,3 +15,16 @@ alias media="sudo mount -t nfs 10.0.0.5:/media /mnt/media/"
 
 alias generations="sudo nix-env --list-generations --profile /nix/var/nix/profiles/system"
 alias rebuild="sudo nixos-rebuild switch"
+
+# Check if TMUX is unset or empty and if the session is interactive
+if test -z "$TMUX" && status is-interactive
+  # Check if any Tmux sessions exist
+  set tmux_sessions (tmux list-sessions)
+  if test -z "$tmux_sessions"
+    # If no sessions exist, start a new Tmux session
+    tmux new-session
+  else
+    # If sessions exist, attach to the first one
+    tmux attach-session -t (echo $tmux_sessions[1] | cut -d: -f1)
+  end
+end
